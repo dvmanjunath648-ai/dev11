1a22b8f307d0f316dfe337931d730902c3d611d0
dvmanjunath648-ai <dvmanjunath648@gmail.com>
2026-02-27 18:51:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("login").addEventListener("click", function() {
+    window.location.href = "login.html"; // make sure report.html is in the same folder
+});
+
+document.getElementById("rep-btn").addEventListener("click", function() {
+    window.location.href = "report.html"; // make sure report.html is in the same folder
+});
+
+document.getElementById("signup").addEventListener("click", function() {
+    window.location.href = "signup.html"; // make sure report.html is in the same folder
+});
